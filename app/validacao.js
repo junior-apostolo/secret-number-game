1cf8b194d5bf60579d894119d0e24283a9ab4c6a
Sergio Borges <sergio.borgesjunior@everis.nttdata.com>
2023-02-23 14:52:05 -0300

    adicionando game over

--- app/validacao.js
+++ app/validacao.js
@@ -1,16 +1,26 @@
 function verificaValorValido(chute) {
-  const numero = +chute
+  const numero = +chute;
 
   if (chuteInvalido(numero)) {
-    elementoChute.innerHTML += "<div>Valor Invalido</div>"
-    return
+    if (chute.toUpperCase() === "GAME OVER") {
+      document.body.innerHTML = `
+        <h2>Game Over!!</h2>
+        <h3>Pressione o botão para jogar novamente</h3>
+
+        <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
+      `;
+      document.body.style.backgroundColor = "black";
+    } else {
+      elementoChute.innerHTML += "<div>Valor Invalido</div>";
+    }
+    return;
   }
 
   if (validaIntervalo(numero)) {
     elementoChute.innerHTML += ` 
     </div>valor invalido: Fale um numero entre ${menorValor} e ${maiorValor}
-    <div>`
-    return
+    <div>`;
+    return;
   }
 
   if (numero === numeroSecreto) {
@@ -19,28 +29,28 @@ function verificaValorValido(chute) {
       <h3>O numero secreto é ${numeroSecreto}</h3>
 
       <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
-    `
+    `;
   } else if (numero > numeroSecreto) {
     elementoChute.innerHTML += `
       <div class="size">O numero secreto é menor <i class="fa-solid fa-arrow-down"></i></div> -->
-    `
+    `;
   } else {
     elementoChute.innerHTML += `
       <div class="size">O numero secreto é maior <i class="fa-solid fa-arrow-up"></i></div> -->
-    `
+    `;
   }
 }
 
 function validaIntervalo(numero) {
-  return numero > maiorValor || numero < menorValor
+  return numero > maiorValor || numero < menorValor;
 }
 
 function chuteInvalido(numero) {
-  return Number.isNaN(numero)
+  return Number.isNaN(numero);
 }
 
-document.body.addEventListener('click', e => {
-  if(e.target.id == 'jogar-novamente') {
-    window.location.reload()
+document.body.addEventListener("click", (e) => {
+  if (e.target.id == "jogar-novamente") {
+    window.location.reload();
   }
-})
+});
